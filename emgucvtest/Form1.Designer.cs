61df57b1a79461902c17cc112a3e4ca377bec07a
ycchen <ycchen@utechzone.com.tw>
2017-02-16 13:35:50 +0800

    s

---
+++ emgucvtest/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace emgucvtest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.OpenImage = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.TbOrdRow = new System.Windows.Forms.TextBox();
+            this.TbOrdCol = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TbGrayLevel = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.TbBlue = new System.Windows.Forms.TextBox();
+            this.TbRed = new System.Windows.Forms.TextBox();
+            this.TbGreen = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // OpenImage
+            // 
+            this.OpenImage.Location = new System.Drawing.Point(16, 35);
+            this.OpenImage.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.OpenImage.Name = "OpenImage";
+            this.OpenImage.Size = new System.Drawing.Size(100, 29);
+            this.OpenImage.TabIndex = 0;
+            this.OpenImage.Text = "BtnOpenImage";
+            this.OpenImage.UseVisualStyleBackColor = true;
+            this.OpenImage.Click += new System.EventHandler(this.OpenImage_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(4, 4);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(40, 27);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(155, 35);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1235, 711);
+            this.panel1.TabIndex = 2;
+            // 
+            // TbOrdRow
+            // 
+            this.TbOrdRow.Location = new System.Drawing.Point(16, 115);
+            this.TbOrdRow.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.TbOrdRow.Name = "TbOrdRow";
+            this.TbOrdRow.Size = new System.Drawing.Size(48, 25);
+            this.TbOrdRow.TabIndex = 3;
+            // 
+            // TbOrdCol
+            // 
+            this.TbOrdCol.Location = new System.Drawing.Point(95, 115);
+            this.TbOrdCol.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.TbOrdCol.Name = "TbOrdCol";
+            this.TbOrdCol.Size = new System.Drawing.Size(51, 25);
+            this.TbOrdCol.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 80);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "row";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(105, 80);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "col";
+            // 
+            // TbGrayLevel
+            // 
+            this.TbGrayLevel.Location = new System.Drawing.Point(24, 168);
+            this.TbGrayLevel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.TbGrayLevel.Name = "TbGrayLevel";
+            this.TbGrayLevel.Size = new System.Drawing.Size(99, 25);
+            this.TbGrayLevel.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(45, 149);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 15);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "graylevel";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(21, 207);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(14, 15);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "b";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(105, 207);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(12, 15);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "r";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(63, 207);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(14, 15);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "g";
+            // 
+            // TbBlue
+            // 
+            this.TbBlue.Location = new System.Drawing.Point(12, 225);
+            this.TbBlue.Name = "TbBlue";
+            this.TbBlue.Size = new System.Drawing.Size(35, 25);
+            this.TbBlue.TabIndex = 11;
+            // 
+            // TbRed
+            // 
+            this.TbRed.Location = new System.Drawing.Point(95, 225);
+            this.TbRed.Name = "TbRed";
+            this.TbRed.Size = new System.Drawing.Size(35, 25);
+            this.TbRed.TabIndex = 12;
+            // 
+            // TbGreen
+            // 
+            this.TbGreen.Location = new System.Drawing.Point(53, 225);
+            this.TbGreen.Name = "TbGreen";
+            this.TbGreen.Size = new System.Drawing.Size(35, 25);
+            this.TbGreen.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1405, 761);
+            this.Controls.Add(this.TbGreen);
+            this.Controls.Add(this.TbRed);
+            this.Controls.Add(this.TbBlue);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.TbGrayLevel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TbOrdCol);
+            this.Controls.Add(this.TbOrdRow);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.OpenImage);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button OpenImage;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox TbOrdRow;
+        private System.Windows.Forms.TextBox TbOrdCol;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TbGrayLevel;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox TbBlue;
+        private System.Windows.Forms.TextBox TbRed;
+        private System.Windows.Forms.TextBox TbGreen;
+    }
+}
+
---
+++ emgucvtest/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.CvEnum;
+
+namespace emgucvtest
+{
+    //Image to Mat :Mat img= new Mat(); img=image.Mat.Clone();
+    //Mat to Image :Image<Gray, Byte> image= img.ToImage<Gray, Byte>();
+    
+    public partial class Form1 : Form
+    {
+        Boolean bload;
+        Mat img = new Mat();
+        Mat imgori = new Mat();
+        Mat imgobj = new Mat();
+        Mat imgtemplate = new Mat();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private double logKernelElementWeight(int radius, double standarddeviation)
+        {
+           Console.WriteLine((0.5*((radius * radius - 2 * standarddeviation * standarddeviation) / Math.Pow(standarddeviation, 4)) * Math.Exp(-((radius * radius) / (2 * standarddeviation * standarddeviation)))).ToString());
+           return 0;
+        }
+        private int[] logKernel(double standarddeviation)
+        {
+            int[] kernel = new int[15] { (int)logKernelElementWeight(7, standarddeviation), (int)logKernelElementWeight(6, standarddeviation), (int)logKernelElementWeight(5, standarddeviation), (int)logKernelElementWeight(4, standarddeviation), (int)logKernelElementWeight(3, standarddeviation), (int)logKernelElementWeight(2, standarddeviation), (int)logKernelElementWeight(1, standarddeviation), (int)logKernelElementWeight(0, standarddeviation), (int)logKernelElementWeight(1, standarddeviation), (int)logKernelElementWeight(2, standarddeviation), (int)logKernelElementWeight(3, standarddeviation), (int)logKernelElementWeight(4, standarddeviation), (int)logKernelElementWeight(5, standarddeviation), (int)logKernelElementWeight(6, standarddeviation), (int)logKernelElementWeight(7, standarddeviation) };
+            return kernel;
+        }
+        private void SetPixelValue(ref Mat m, int row, int col, Byte v)
+        {
+            Image<Gray, Byte> image = m.ToImage<Gray, Byte>();
+            image.Data[row, col, 0] = v;
+            m = image.Mat.Clone();
+            image.Dispose();
+        }
+        private void SetPixelValue(ref Mat m, int row, int col, int component, Byte v)
+        {
+            Image<Bgr, Byte> image = m.ToImage<Bgr, Byte>();
+            image.Data[row, col, component] = v;
+            m = image.Mat.Clone();
+            image.Dispose();
+        }
+        private Byte GetPixelValue(Mat m, int row, int col)
+        {
+            Image<Gray,byte> im= m.ToImage<Gray, Byte>();
+            byte b=im.Data[row,col,0];
+            im.Dispose();
+            return b;
+        }
+        private Byte GetPixelValue(Mat m, int row, int col, int component)
+        {
+            Image<Bgr, Byte> im = m.ToImage<Bgr, Byte>();
+            byte b = im.Data[row, col, component];
+            im.Dispose();
+            return b;
+        }
+        private double yenStandardDeviation(List<double> doubleList)
+        {
+            double average = doubleList.Average();
+            double std = 0;
+            for (int i = 0; i < doubleList.Count(); i++)
+			{
+                std += Math.Abs(doubleList[i] - average);
+			}
+            std /= doubleList.Count();
+            return std;
+        }
+        //private double yenCorrelationCoefficient(Mat img1, Mat img2)
+        //{
+        //    double correlationcoefficient = 0d;
+        //    double regionaverage = 0d;
+        //    double templateaverage = 0d;
+        //    double numerator = 0d;
+        //    double denominatorL = 0d;
+        //    double denominatorR = 0d;
+        //    for (int row = 0; row < 0   ; row++)
+        //    {
+        //        for (int col = 0; col < 0; col++)
+        //        {
+        //            numerator += ((GetPixelValue(imgori, row, col) - regionaverage) * (GetPixelValue(imgtemplate, row, col) - templateaverage));
+        //        }
+        //    }
+        //    return 0;
+        //}
+        private void OpenImage_Click(object sender, EventArgs e)
+        {
+            //#region open original image
+            //OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            //openFileDialog1.Filter = "Image Files (*.png;*.tif;*.jpg;*.bmp)|*.png;*.tif;*.jpg;*.bmp|All files (*.*)|*.*";
+            //if (openFileDialog1.ShowDialog() == DialogResult.Cancel) return;
+            //imgori = CvInvoke.Imread(openFileDialog1.FileName, LoadImageType.Unchanged);
+            //#endregion
+            //#region open object image
+            //OpenFileDialog openFileDialog2 = new OpenFileDialog();
+            //openFileDialog2.Filter = "Image Files (*.png;*.tif;*.jpg;*.bmp)|*.png;*.tif;*.jpg;*.bmp|All files (*.*)|*.*";
+            //if (openFileDialog2.ShowDialog() == DialogResult.Cancel) return;
+            //imgobj = CvInvoke.Imread(openFileDialog2.FileName, LoadImageType.Unchanged);
+            //#endregion
+            int[] ker=new int[15];
+            ker = logKernel(2);
+            for (int i = 0; i < 15; i++)
+            {
+             //   Console.WriteLine(ker[i].ToString());
+            }
+            pictureBox1.Image = imgori.Bitmap;
+            bload = true;
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(bload)
+            {
+                TbOrdRow.Text = e.Y.ToString();
+                TbOrdCol.Text = e.X.ToString();
+                if (img.NumberOfChannels == 3)
+                {
+                    TbBlue.Text = img.ToImage<Bgr, Byte>().Data[e.Y, e.X, 0].ToString();
+                    TbGreen.Text = img.ToImage<Bgr, Byte>().Data[e.Y, e.X, 1].ToString();
+                    TbRed.Text = img.ToImage<Bgr, Byte>().Data[e.Y, e.X, 2].ToString();
+                }
+                else if (img.NumberOfChannels == 1)
+                {
+                    TbGrayLevel.Text = img.ToImage<Gray, Byte>().Data[e.Y, e.X, 0].ToString();
+                }
+            }
+        }
+    }
+}
